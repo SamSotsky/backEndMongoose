eb310a17610b54b5abbf967f43832b1f1241df93
SamSotsky <sam.sotsky01@gmail.com>
2022-05-16 15:14:02 -0700

    first commit

---
+++ jokesAPI/server/models/joke.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+
+const JokeSchema = new mongoose.Schema({
+    setup: {
+        type: String,
+        minlength: [10, "Setup must be at least 10 characters"]
+    },
+    punchline:{
+        type: String,
+        minlength: [3, "Punchline must be at least 3 characters"]
+    },
+}, {timestamps: true})
+
+
+
+const Joke = mongoose.model('Joke', JokeSchema);
+
+module.exports = Joke;
